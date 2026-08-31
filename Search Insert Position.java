aef3b6aad25c59d21861cc96f78f8f1f1be9fa89
anwesha-webdev <anweshabhattacharjee21@gmail.com>
2026-09-01 03:12:19 +0530

    Add searchInsert method in Solution class
    Implement searchInsert method to find the target's position or insertion point in a sorted array.

---
+++ Search Insert Position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) 
+    {
+        int c=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                c=i;
+                break;
+            }
+            else{
+                if(target>nums[i])
+                {
+                    c=i+1;
+                }
+            }
+        }
+        return c;
+    }
+}
