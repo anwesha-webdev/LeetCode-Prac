bcf647fef6c526a275d442144bb8af70d0883fce
anwesha-webdev <anweshabhattacharjee21@gmail.com>
2026-09-08 20:38:26 +0530

    Implement isIsomorphic method in Solution class

---
+++ Isomorphic Strings.java
@@ -0,0 +1,33 @@
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i + 1; j < s.length(); j++) {
+
+            
+                if (s.charAt(i) == s.charAt(j)) {
+
+                    if (t.charAt(i) != t.charAt(j)) {
+                        return false;
+                    }
+                }
+
+                if (t.charAt(i) == t.charAt(j)) {
+
+                    if (s.charAt(i) != s.charAt(j)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
