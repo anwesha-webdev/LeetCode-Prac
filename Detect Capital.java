60f8e835c847ab7e0bbedf04f09141c5eadb7496
anwesha-webdev <anweshabhattacharjee21@gmail.com>
2026-09-10 12:50:36 +0530

    Implement detectCapitalUse method

---
+++ Detect Capital.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    public boolean detectCapitalUse(String word)
+    {
+        String upperWord = word.toUpperCase();
+        String lower = word.toLowerCase();
+        String onecap = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+
+        if(word.equals(upperWord))
+        {
+            return true;
+        }
+        else if(word.equals(lower))
+        {
+            return true;
+        }
+        else if(word.equals(onecap))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
