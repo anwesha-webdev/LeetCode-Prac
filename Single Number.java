713a33bceebb71708a137a88f2525c2cb655b533
anwesha-webdev <anweshabhattacharjee21@gmail.com>
2026-09-21 05:05:24 +0530

    Implement single number finder in Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+        int no=0;
+        for(int i=0;i<nums.length;i++)
+        {
+             int c=0;
+            for(int j=0;j<nums.length;j++)
+            {
+               
+                if(nums[i]==nums[j])
+                {
+                    c++;
+                }
+               
+            }
+             if(c==1)
+                {
+                    no=nums[i];
+                }
+        }
+        return no;
+    }
+}
